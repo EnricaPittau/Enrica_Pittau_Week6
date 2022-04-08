bc028e890f9141008d7e92175435bae8e3f3df76
EnricaPittau <101341843+EnricaPittau@users.noreply.github.com>
2022-04-08 15:32:48 +0200

    Add files via upload

---
+++ EnricaPittauWeek6/Agente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnricaPittauWeek6
+{
+    internal class Agente : Persona
+    {
+        public string AreaGeografica { get; set; }
+        public int AnnoDiInizioAttivita { get; set; }
+        public Agente()
+        {
+
+        }
+        public Agente(string nome, string cognome, string codiceFiscaleDellAgente, string areaGeografica, int annoDiInizioAttivita )
+            :base(codiceFiscaleDellAgente, nome, cognome)
+        {
+            AreaGeografica = areaGeografica;
+            AnnoDiInizioAttivita = annoDiInizioAttivita;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" Anni di servizio: {CalcolaAnniServizio()}";
+        }
+        public int CalcolaAnniServizio()
+        {
+            int anniDiServizio = DateTime.Now.Year - AnnoDiInizioAttivita;
+            return anniDiServizio;
+        }
+    }
+}
---
+++ EnricaPittauWeek6/IRepositoryGestioneAgenti.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnricaPittauWeek6
+{
+    internal interface IRepositoryGestioneAgenti
+    {
+        void GetAllAgenti();//Mostrare tutti gli agenti di polizia
+        void GetAgenteByArea(string areaGeografica);//Scelta un’area geografica, mostrare gli agenti assegnati a quell’area
+        void GetAgenteByAnniServizio(int anniServizio);//Scelti gli anni di servizio, mostrare gli agenti con anni di servizio maggiori o uguali rispetto all’input
+
+        void InsertNewAgente(string codiceFiscaleDellAgente, string nome, string cognome, string areaGeografica, int annoDiInizioAttivita);//Inserire un nuovo agente solo se non è già presente nel database
+    }
+}
